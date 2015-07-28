cf13119e03171809e9eb6bdb992b0ce6bbb72033
Andrew Gregory <angregor@asu.edu>
2015-07-28 00:28:38 -0700

    fixed lesson 8 now complete

--- lesson8.js
+++ lesson8.js
@@ -1,13 +1,15 @@
 var bl = require('bl');
 var http = require('http');
 
-http.get(process.argv[1], function callback(response) {
-	response.on("data", response.pipe(bl(function (err, data) {
-		var data = data.toString();
+http.get(process.argv[2], function callback(response) {
+response.pipe(bl(function (err, data) {
+
+		if(err) {
+			return console.log("");
+		}
+
+		data = data.toString();
 		console.log(data.length);
 		console.log(data);
-	})););
-	response.on("error", function callback(error) {
-		console.log(error);
-	})
+	}));
 });
